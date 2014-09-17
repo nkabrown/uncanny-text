4323ebc580bf440aa9e19e9a7b871716b4d70823
Nathan Brown <nkabrown@gmail.com>
2014-09-17 11:21:05 -0400

    Successful fetching of the keyup event

--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
 	$("#input").on('keyup', function(e) {
-		
-			console.log(e);
+		console.log(e);
+		$(this).val();
+		var text = $(this).val();
+		console.log(text);
+			//Fetch the value of the keyup event 
+			//insert that value into my <div> with the .html() method
+			//think about how I want the text to be transformed...fadeIn, upside down,
 	})
 
 
